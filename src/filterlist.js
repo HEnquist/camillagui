df49053ce0a08bec5b6a9f794b215ce3413b2869
JWahle <jwahle@github.com>
2021-01-29 22:22:54 +0100

    Changed default ramp_time of loudness and volume filters to 200

--- src/filterlist.js
+++ src/filterlist.js
@@ -99,10 +99,10 @@ class FilterParams extends React.Component {
       Default: { gain: 0.0, inverted: false },
     },
     Volume: {
-      Default: { ramp_time: 0.2 },
+      Default: { ramp_time: 200 },
     },
     Loudness: {
-      Default: { reference_level: 0.0, high_boost: 10, low_boost: 10, ramp_time: 0.2 },
+      Default: { reference_level: 0.0, high_boost: 10, low_boost: 10, ramp_time: 200 },
     },
     DiffEq: {
       Default: { a: [1.0, 0.0], b: [1.0, 0.0] },
@@ -232,8 +232,8 @@ class Filter extends React.Component {
     },
     Delay: { delay: 0.0, unit: "ms" },
     Gain: { gain: 0.0, inverted: false },
-    Volume: { ramp_time: 0.2 },
-    Loudness: { reference_level: 0.0, high_boost: 10, low_boost: 10, ramp_time: 0.2 },
+    Volume: { ramp_time: 200 },
+    Loudness: { reference_level: 0.0, high_boost: 10, low_boost: 10, ramp_time: 200 },
     DiffEq: { a: [1.0, 0.0], b: [1.0, 0.0] },
     Dither: { type: "Simple", bits: 16 },
   };
