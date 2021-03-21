aca65f1b1c49be653febce43398204ed991f48b1
JWahle <jwahle@github.com>
2021-03-21 15:26:59 +0100

    Volume is updated on page load (set to 0, if CDSP is not connected)

--- src/sidepanel.tsx
+++ src/sidepanel.tsx
@@ -180,8 +180,12 @@ export class SidePanel extends React.Component<
 
   private async updateVolume() {
     const vol_req = await fetch("/api/getparam/volume")
-    const volume = await vol_req.text()
-    this.setState({ volume: volume })
+    let volume = '0'
+    try {
+      if (vol_req.ok)
+        volume = parseInt(await vol_req.text(), 10).toString()
+    } catch (e) {}
+    this.setState({volume: volume})
   }
 
   private async setVolume(value: string) {
