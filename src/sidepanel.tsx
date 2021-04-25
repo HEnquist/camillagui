5a063d299014786386cc4e87e4f6520e34f766f8
JWahle <jwahle@github.com>
2021-04-25 13:49:27 +0200

    Introduced /api/getstatus endpoint to retrieve multiple parameters in one JSON request

--- src/sidepanel.tsx
+++ src/sidepanel.tsx
@@ -52,31 +52,32 @@ class ConfigCheckMessage extends React.Component<
   }
 }
 
-interface Version { major: number, minor: number, patch: number }
-
-type SidePanelProps = {
-  config: Config,
-  setConfig: (config: Config) => void,
-  currentConfigFile?: string,
+interface SidePanelProps {
+  config: Config
+  setConfig: (config: Config) => void
+  currentConfigFile?: string
   setCurrentConfig: (filename: string, config: Config) => void
+}
+
+interface Status {
+  cdsp_status: string
+  capturesignalrms: number[]
+  playbacksignalrms: number[]
+  capturerate: number | ''
+  rateadjust: number | ''
+  bufferlevel: number | ''
+  clippedsamples: number | ''
+  cdsp_version: string
+  py_cdsp_version: string
+  backend_version: string
 }
 
 export class SidePanel extends React.Component<
   SidePanelProps,
-  {
-    clearTimer: () => void,
-    msg: string,
-    capture_rms: number[],
-    playback_rms: number[],
-    state: string,
-    rateadjust: number | '',
-    capturerate: number | '',
-    bufferlevel: number | '',
-    nbrclipped: number | '',
-    clipped: boolean,
-    dsp_ver: Version | null,
-    pylib_ver: Version | null,
-    backend_ver: Version | null,
+  Status & {
+    clearTimer: () => void
+    msg: string
+    clipped: boolean
   }
 > {
 
@@ -85,17 +86,17 @@ export class SidePanel extends React.Component<
     this.state = {
       clearTimer: () => {},
       msg: '',
-      capture_rms: [],
-      playback_rms: [],
-      state: 'backend offline',
+      capturesignalrms: [],
+      playbacksignalrms: [],
+      cdsp_status: 'backend offline',
       rateadjust: '',
       capturerate: '',
       bufferlevel: '',
-      nbrclipped: '',
+      clippedsamples: '',
       clipped: false,
-      dsp_ver: null,
-      pylib_ver: null,
-      backend_ver: null,
+      cdsp_version: '',
+      py_cdsp_version: '',
+      backend_version: ''
     }
     this.timer = this.timer.bind(this)
     this.fetchConfig = this.fetchConfig.bind(this)
@@ -104,20 +105,8 @@ export class SidePanel extends React.Component<
 
   async componentDidMount() {
     const intervalId = setInterval(this.timer, 500)
-    this.setState({ clearTimer: () => {  clearInterval(intervalId) } })
-    try {
-      const dsp_ver_req = await fetch("/api/version")
-      const pylib_ver_req = await fetch("/api/libraryversion")
-      const backend_ver_req = await fetch("/api/backendversion")
-      const dsp_ver = await dsp_ver_req.json()
-      const pylib_ver = await pylib_ver_req.json()
-      const backend_ver = await backend_ver_req.json()
-      this.setState({
-        dsp_ver: dsp_ver,
-        pylib_ver: pylib_ver,
-        backend_ver: backend_ver,
-      })
-    } catch (err) {}
+    this.setState({clearTimer: () => clearInterval(intervalId)})
+    this.timer()
     this.loadCurrentConfig()
   }
 
@@ -125,41 +114,29 @@ export class SidePanel extends React.Component<
     this.state.clearTimer()
   }
 
+  offline_states = ['backend offline', 'offline']
+
   private async timer() {
-    const state_req = await fetch("/api/getparam/state")
-    const processingstate = await state_req.text()
-    let capture_rms: number[] = []
-    let playback_rms: number[] = []
-    let capturerate: number | '' = ''
-    let rateadjust: number | '' = ''
-    let bufferlevel: number | '' = ''
-    let nbrclipped: number | '' = ''
+    let status: Status
     try {
-      const capt_rms_req = await fetch("/api/getlistparam/capturesignalrms")
-      const pb_rms_req = await fetch("/api/getlistparam/playbacksignalrms")
-      const capturerate_req = await fetch("/api/getparam/capturerate")
-      const rateadjust_req = await fetch("/api/getparam/rateadjust")
-      const bufferlevel_req = await fetch("/api/getparam/bufferlevel")
-      const nbrclipped_req = await fetch("/api/getparam/clippedsamples")
-      capture_rms = await capt_rms_req.json()
-      playback_rms = await pb_rms_req.json()
-      capturerate = parseInt(await capturerate_req.text())
-      rateadjust = parseFloat(await rateadjust_req.text())
-      bufferlevel = parseInt(await bufferlevel_req.text())
-      nbrclipped = parseInt(await nbrclipped_req.text())
+      status = await (await fetch("/api/status")).json()
     } catch (err) {
-      console.log("camilladsp offline")
+      status = {
+        cdsp_status: 'backend offline',
+        capturesignalrms: [],
+        playbacksignalrms: [],
+        capturerate: '',
+        rateadjust: '',
+        bufferlevel: '',
+        clippedsamples: '',
+        cdsp_version: '',
+        py_cdsp_version: '',
+        backend_version: '',
+      }
     }
-    console.log(processingstate, capturerate, rateadjust)
-    this.setState((state) => ({
-      state: processingstate,
-      capture_rms: capture_rms,
-      playback_rms: playback_rms,
-      capturerate: capturerate,
-      rateadjust: rateadjust,
-      bufferlevel: bufferlevel,
-      nbrclipped: nbrclipped,
-      clipped: state.nbrclipped >= 0 && nbrclipped > state.nbrclipped,
+    this.setState((oldState) => ({
+      ...status,
+      clipped: oldState.clippedsamples >= 0 && status.clippedsamples > oldState.clippedsamples,
     }))
   }
 
@@ -194,21 +171,22 @@ export class SidePanel extends React.Component<
   }
 
   render() {
-    const activeConfigFile = this.props.currentConfigFile;
+    const activeConfigFile = this.props.currentConfigFile
+    const cdsp_state = this.state.cdsp_status
     return (
       <section className="tabpanel" style={{width: '250px'}}>
         <img src={camillalogo} alt="graph" width="100%" height="100%" />
-        <VolumeBox
-            capture_rms={this.state.capture_rms}
-            playback_rms={this.state.playback_rms}
+        {!this.offline_states.includes(cdsp_state) && <VolumeBox
+            capture_rms={this.state.capturesignalrms}
+            playback_rms={this.state.playbacksignalrms}
             clipped={this.state.clipped}
-            setMessage={message => this.setState({msg: message})}/>
+            setMessage={message => this.setState({msg: message})}/>}
         <Box title="CamillaDSP">
           <div className="two-column-grid">
-            <div className="alignRight">State:</div><div>{this.state.state}</div>
+            <div className="alignRight">State:</div><div>{cdsp_state}</div>
             <div className="alignRight">Capture samplerate:</div><div>{this.state.capturerate}</div>
             <div className="alignRight">Rate adjust:</div><div>{this.state.rateadjust}</div>
-            <div className="alignRight">Clipped samples:</div><div>{this.state.nbrclipped}</div>
+            <div className="alignRight">Clipped samples:</div><div>{this.state.clippedsamples}</div>
             <div className="alignRight">Buffer level:</div><div>{this.state.bufferlevel}</div>
             <div className="alignRight">Message:</div><div>{this.state.msg}</div>
           </div>
@@ -239,18 +217,11 @@ export class SidePanel extends React.Component<
           <ConfigCheckMessage config={this.props.config} />
         </Box>
         <div className="versions">
-          <div>{SidePanel.version('CamillaDSP', this.state.dsp_ver)}</div>
-          <div>{SidePanel.version('pyCamillaDSP', this.state.pylib_ver)}</div>
-          <div>{SidePanel.version('Backend', this.state.backend_ver)}</div>
+          <div>CamillaDSP {this.state.cdsp_version}</div>
+          <div>pyCamillaDSP {this.state.py_cdsp_version}</div>
+          <div>Backend {this.state.backend_version}</div>
         </div>
       </section>
     )
-  }
-
-  private static version(label: string, version: Version | null) {
-    if (!version)
-      return ''
-    const {major, minor, patch} = version
-    return `${label} ${major}.${minor}.${patch}`
   }
 }
