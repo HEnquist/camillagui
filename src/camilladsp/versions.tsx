c71b3857d67acf28f793337828d8186ff50bba07
HEnquist <henrik.enquist@gmail.com>
2024-10-15 21:38:44 +0200

    Better component version display

--- src/camilladsp/versions.tsx
+++ src/camilladsp/versions.tsx
@@ -11,7 +11,34 @@ export interface Versions {
 
 export function VersionLabels(props: {versions: Status}) {
   const {cdsp_version, py_cdsp_version, py_cdsp_plot_version, backend_version} = props.versions
-  let tooltip = "CamillaDSP: " + cdsp_version + "<br>pyCamillaDSP: " + py_cdsp_version + "<br>pyCamillaDSP-plot: " + py_cdsp_plot_version + "<br>GUI backend: " + backend_version
+  let tooltip = `
+    <table>
+      <tr>
+        <th class=namecol>Component</th>
+        <th class=valuecol>Version</th>
+      </tr>
+      <tr>
+        <td class=namecol>CamillaDSP</td>
+        <td class=valuecol>${cdsp_version}</td>
+      </tr>
+      <tr>
+        <td class=namecol>CamillaGUI frontend</td>
+        <td class=valuecol>${version}</td>
+      </tr>
+      <tr>
+        <td class=namecol>CamillaGUI backend</td>
+        <td class=valuecol>${backend_version}</td>
+      </tr>
+      <tr>
+        <td class=namecol>pyCamillaDSP</td>
+        <td class=valuecol>${py_cdsp_version}</td>
+      </tr>
+      <tr>
+        <td class=namecol>pyCamillaDSP-plot</td>
+        <td class=valuecol>${py_cdsp_plot_version}</td>
+      </tr>
+    </table>
+  `
   return <div  data-tooltip-html={tooltip} data-tooltip-id="main-tooltip" className="versions">
     <div>CamillaGUI version {version}</div>
   </div>
